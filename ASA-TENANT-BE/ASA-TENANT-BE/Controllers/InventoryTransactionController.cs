7b9ba3987633186f1c26f70618f37afb6f1d65cf
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-09-01 11:43:38 +0700

    inventory transaction crud

---
+++ ASA-TENANT-BE/ASA-TENANT-BE/Controllers/InventoryTransactionController.cs
@@ -0,0 +1,54 @@
+﻿using ASA_TENANT_REPO.Models;
+using ASA_TENANT_SERVICE.DTOs.Request;
+using ASA_TENANT_SERVICE.DTOs.Response;
+using ASA_TENANT_SERVICE.Implenment;
+using ASA_TENANT_SERVICE.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASA_TENANT_BE.Controllers
+{
+    [Route("api/inventory-transactions")]
+    [ApiController]
+    public class InventoryTransactionController : ControllerBase
+    {
+        private readonly IInventoryTransactionService _inventoryTransactionService;
+
+        public InventoryTransactionController(IInventoryTransactionService inventoryTransactionService)
+        {
+            _inventoryTransactionService = inventoryTransactionService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<InventoryTransaction>>> GetFiltered([FromQuery] InventoryTransactionGetRequest requestDto, [FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+        {
+            try
+            {
+                var result = await _inventoryTransactionService.GetFilteredInventoryTransactionsAsync(requestDto, page, pageSize);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult<InventoryTransactionResponse>> Create([FromBody] InventoryTransactionRequest request)
+        {
+            var result = await _inventoryTransactionService.CreateAsync(request);
+            return Ok(result);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<InventoryTransactionResponse>> Update(long id, [FromBody] InventoryTransactionRequest request)
+        {
+            var result = await _inventoryTransactionService.UpdateAsync(id, request);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> Delete(long id)
+        {
+            var result = await _inventoryTransactionService.DeleteAsync(id);
+            return Ok(result);
+        }
+    }
+}
---
+++ ASA-TENANT-BE/ASA-TENANT-SERVICE/DTOs/Request/InventoryTransactionRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASA_TENANT_SERVICE.DTOs.Request
+{
+    public class InventoryTransactionRequest
+    {
+        public int? Type { get; set; }
+        public long ProductId { get; set; }
+        public long? OrderId { get; set; }
+        public long UnitId { get; set; }
+        public int Quantity { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal? Price { get; set; }
+        public long ShopId { get; set; }
+    }
+
+    public class InventoryTransactionGetRequest
+    {
+        public long InventoryTransactionId { get; set; }
+        public int? Type { get; set; }
+        public long ProductId { get; set; }
+        public long? OrderId { get; set; }
+        public long UnitId { get; set; }
+        public int Quantity { get; set; }
+        public string? ImageUrl { get; set; }
+        public decimal? Price { get; set; }
+        public long ShopId { get; set; }
+    }
+}

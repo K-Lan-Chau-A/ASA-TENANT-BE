cb2403309c34cf0b3115e25f011ff53346be0540
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-09-01 12:18:32 +0700

    promotion crud

---
+++ ASA-TENANT-BE/ASA-TENANT-BE/Controllers/PromotionController.cs
@@ -0,0 +1,54 @@
+﻿using ASA_TENANT_REPO.Models;
+using ASA_TENANT_SERVICE.DTOs.Request;
+using ASA_TENANT_SERVICE.DTOs.Response;
+using ASA_TENANT_SERVICE.Implenment;
+using ASA_TENANT_SERVICE.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASA_TENANT_BE.Controllers
+{
+    [Route("api/promotions")]
+    [ApiController]
+    public class PromotionController : ControllerBase
+    {
+        private readonly IPromotionService _promotionService;
+        public PromotionController(IPromotionService promotionService)
+        {
+            _promotionService = promotionService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Promotion>>> GetFiltered([FromQuery] PromotionGetRequest requestDto, [FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+        {
+            try
+            {
+                var result = await _promotionService.GetFilteredPromotionsAsync(requestDto, page, pageSize);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult<PromotionResponse>> Create([FromBody] PromotionRequest request)
+        {
+            var result = await _promotionService.CreateAsync(request);
+            return Ok(result);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<PromotionResponse>> Update(long id, [FromBody] PromotionRequest request)
+        {
+            var result = await _promotionService.UpdateAsync(id, request);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> Delete(long id)
+        {
+            var result = await _promotionService.DeleteAsync(id);
+            return Ok(result);
+        }
+
+    }
+}
--- ASA-TENANT-BE/ASA-TENANT-SERVICE/Interface/IPromotionService.cs
+++ ASA-TENANT-BE/ASA-TENANT-SERVICE/Interface/IPromotionService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ASA_TENANT_SERVICE.DTOs.Common;
+using ASA_TENANT_SERVICE.DTOs.Request;
+using ASA_TENANT_SERVICE.DTOs.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,9 @@ namespace ASA_TENANT_SERVICE.Interface
 {
     public interface IPromotionService
     {
+        Task<PagedResponse<PromotionResponse>> GetFilteredPromotionsAsync(PromotionGetRequest Filter, int page, int pageSize);
+        Task<ApiResponse<PromotionResponse>> CreateAsync(PromotionRequest request);
+        Task<ApiResponse<PromotionResponse>> UpdateAsync(long id, PromotionRequest request);
+        Task<ApiResponse<bool>> DeleteAsync(long id);
     }
 }

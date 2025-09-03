57ba5066746c75d684aacf93725519cbe2f8d838
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-09-04 00:02:03 +0700

    check valid activation shopId to login + jwt token

---
+++ ASA-TENANT-BE/ASA-TENANT-SERVICE/DTOs/Request/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASA_TENANT_SERVICE.DTOs.Request
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ASA-TENANT-BE/ASA-TENANT-SERVICE/Interface/IAuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿using ASA_TENANT_SERVICE.DTOs.Common;
+using ASA_TENANT_SERVICE.DTOs.Request;
+using ASA_TENANT_SERVICE.DTOs.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASA_TENANT_SERVICE.Interface
+{
+    public interface IAuthenticationService
+    {
+        Task<ApiResponse<LoginResponse>> CheckLogin(LoginRequest loginRequest);
+    }
+}

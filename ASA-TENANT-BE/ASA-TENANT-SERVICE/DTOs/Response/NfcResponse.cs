ad1eb5736cc5d41061b8f4500ffe94a0c7b9973c
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-09-05 15:20:19 +0700

    nfc crud

---
+++ ASA-TENANT-BE/ASA-TENANT-SERVICE/DTOs/Response/NfcResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASA_TENANT_SERVICE.DTOs.Response
+{
+    public class NfcResponse
+    {
+        public long NfcId { get; set; }
+        public short? Status { get; set; }
+        public decimal? Balance { get; set; }
+        public long? CustomerId { get; set; }
+        public string NfcCode { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? LastUsedDate { get; set; }
+        public string? CustomerFullName { get; set; }
+        public string? CustomerPhone { get; set; }
+        public string? CustomerEmail { get; set; }
+        public string? CustomerRank { get; set; }
+
+    }
+}

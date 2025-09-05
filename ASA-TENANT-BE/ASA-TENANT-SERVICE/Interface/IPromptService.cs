5e6234cda168cd945b77e9c2437d1aeebb83421c
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-09-05 18:33:18 +0700

    prompt crud

--- ASA-TENANT-BE/ASA-TENANT-SERVICE/Interface/IPromptService.cs
+++ ASA-TENANT-BE/ASA-TENANT-SERVICE/Interface/IPromptService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ASA_TENANT_SERVICE.DTOs.Common;
+using ASA_TENANT_SERVICE.DTOs.Request;
+using ASA_TENANT_SERVICE.DTOs.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,9 @@ namespace ASA_TENANT_SERVICE.Interface
 {
     public interface IPromptService
     {
+        Task<PagedResponse<PromptResponse>> GetFilteredPromptsAsync(PromptGetRequest Filter, int page, int pageSize);
+        Task<ApiResponse<PromptResponse>> CreateAsync(PromptRequest request);
+        Task<ApiResponse<PromptResponse>> UpdateAsync(long id, PromptRequest request);
+        Task<ApiResponse<bool>> DeleteAsync(long id);
     }
 }

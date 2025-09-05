fc445d99e32fed79c6edf5412e5fdcda25ff191f
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-09-05 15:17:55 +0700

    ProductUnit Mappings, Nfc Mappings, Promotion Product Mappings

--- ASA-TENANT-BE/ASA-TENANT-SERVICE/Mapping/MappingProfile.cs
+++ ASA-TENANT-BE/ASA-TENANT-SERVICE/Mapping/MappingProfile.cs
@@ -91,6 +91,35 @@ namespace ASA_TENANT_SERVICE.Mapping
             CreateMap<Fcm, FcmResponse>().ReverseMap();
             CreateMap<FcmRequest, Fcm>().ReverseMap();
             CreateMap<FcmGetRequest, Fcm>().ReverseMap();
+
+            //ProductUnit Mappings
+            CreateMap<ProductUnit, ProductUnitResponse>()
+     .          ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.ProductName))
+                .ForMember(dest => dest.UnitName, opt => opt.MapFrom(src => src.Unit.Name));
+            CreateMap<ProductUnitRequest, ProductUnit>().ReverseMap();
+            CreateMap<ProductUnitGetRequest, ProductUnit>().ReverseMap();
+
+            // Nfc Mappings
+            CreateMap<Nfc, NfcResponse>()
+                .ForMember(dest => dest.CustomerFullName, opt => opt.MapFrom(src => src.Customer.FullName))
+                .ForMember(dest => dest.CustomerEmail, opt => opt.MapFrom(src => src.Customer.Email))
+                .ForMember(dest => dest.CustomerPhone, opt => opt.MapFrom(src => src.Customer.Phone))
+                .ForMember(dest => dest.CustomerRank, opt => opt.MapFrom(src => src.Customer.Rank));
+            CreateMap<NfcRequest, Nfc>().ReverseMap();
+            CreateMap<NfcGetRequest, Nfc>().ReverseMap();
+
+            // Promotion Product Mappings
+            CreateMap<PromotionProduct, PromotionProductResponse>()
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.ProductName))
+                .ForMember(dest => dest.PromotionName, opt => opt.MapFrom(src => src.Promotion.Name))
+                .ForMember(dest => dest.PromotionValue, opt => opt.MapFrom(src => src.Promotion.Value))
+                .ForMember(dest => dest.PromotionType, opt => opt.MapFrom(src => src.Promotion.Type))
+                .ForMember(dest => dest.PromotionStartDate, opt => opt.MapFrom(src => src.Promotion.StartDate))
+                .ForMember(dest => dest.PromotionEndDate, opt => opt.MapFrom(src => src.Promotion.EndDate))
+                .ForMember(dest => dest.PromotionStartTime, opt => opt.MapFrom(src => src.Promotion.StartTime))
+                .ForMember(dest => dest.PromotionEndTime, opt => opt.MapFrom(src => src.Promotion.EndTime));
+            CreateMap<PromotionProductRequest, PromotionProduct>().ReverseMap();
+            CreateMap<PromotionProductGetRequest, PromotionProduct>().ReverseMap();
         }
     }
 }

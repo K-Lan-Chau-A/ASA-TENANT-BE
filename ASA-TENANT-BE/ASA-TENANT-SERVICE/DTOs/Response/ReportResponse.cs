9570fdda0757a7f73f04ecba08d7837fdcc51316
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-09-13 11:56:20 +0700

    get report

---
+++ ASA-TENANT-BE/ASA-TENANT-SERVICE/DTOs/Response/ReportResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASA_TENANT_SERVICE.DTOs.Response
+{
+    public class ReportResponse
+    {
+        public long ReportId { get; set; }
+        public short? Type { get; set; }
+        public DateOnly? StartDate { get; set; }
+        public DateOnly? EndDate { get; set; }
+        public DateTime? CreateAt { get; set; }
+        public decimal? Revenue { get; set; }
+        public int? OrderCounter { get; set; }
+        public long? ShopId { get; set; }
+        public decimal? GrossProfit { get; set; }
+        public decimal? Cost { get; set; }
+        public List<ReportDetailResponse> ReportDetails { get; set; } = new List<ReportDetailResponse>();
+    }
+}
--- ASA-TENANT-BE/ASA-TENANT-SERVICE/Implenment/ReportService.cs
+++ ASA-TENANT-BE/ASA-TENANT-SERVICE/Implenment/ReportService.cs
@@ -1,7 +1,12 @@
 ﻿using ASA_TENANT_REPO.DBContext;
 using ASA_TENANT_REPO.Models;
 using ASA_TENANT_REPO.Repository;
+using ASA_TENANT_SERVICE.DTOs.Common;
+using ASA_TENANT_SERVICE.DTOs.Request;
+using ASA_TENANT_SERVICE.DTOs.Response;
 using ASA_TENANT_SERVICE.Interface;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,20 +18,39 @@ namespace ASA_TENANT_SERVICE.Implenment
     public class ReportService : IReportService
     {
         private readonly ReportRepo _reportRepo;
-        public ReportService(ReportRepo reportRepo)
+        private readonly IMapper _mapper;
+        public ReportService(ReportRepo reportRepo, IMapper mapper)
         {
             _reportRepo = reportRepo;
+            _mapper = mapper;
         }
 
-        public async Task GenerateDailyReportAsync()
+        public async Task GenerateWeeklyReportAsync()
         {
-            await _reportRepo.GenerateDailyReportAsync();
+            await _reportRepo.GenerateWeeklyReportAsync();
 
         }
 
         public async Task GenerateMonthlyReportAsync()
         {
             await _reportRepo.GenerateMonthlyReportAsync();
+        }
+
+        public async Task<PagedResponse<ReportResponse>> GetFilteredReportAsync(ReportGetRequest Filter, int page, int pageSize)
+        {
+            var filter = _mapper.Map<Report>(Filter);
+            var query = _reportRepo.GetFiltered(filter);
+
+            var totalCount = await query.CountAsync();
+            var items = await query.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return new PagedResponse<ReportResponse>
+            {
+                Items = _mapper.Map<IEnumerable<ReportResponse>>(items),
+                TotalCount = totalCount,
+                Page = page,
+                PageSize = pageSize
+            };
         }
     }
 }
